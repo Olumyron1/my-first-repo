9d017607b63c767e0946dc25dc533d55e5589e21
Olumyron2 <etzyrboimyron@gmail.com>
2024-04-29 10:13:46 +0100

    recommited files

---
+++ thirdT/index.js
@@ -0,0 +1,71 @@
+let search = document.querySelector('.search-box')
+document.querySelector('#search-icon').onclick =() => {
+    search.classList.toggle('active')
+    cart.classList.remove('active')
+    user.classList.remove('active')
+    navbar.classList.remove('active')
+}
+
+let cart = document.querySelector('.cart')
+document.querySelector('#cart-icon').onclick =() => {
+    cart.classList.toggle('active')
+    search.classList.remove('active')
+    user.classList.remove('active')
+    navbar.classList.remove('active')
+}
+
+let user = document.querySelector('.user')
+document.querySelector('#user-icon').onclick =() => {
+    user.classList.toggle('active')
+    search.classList.remove('active')
+    cart.classList.remove('active')
+    navbar.classList.remove('active')
+}
+
+let navbar = document.querySelector('.navbar')
+document.querySelector('#menu-icon').onclick =() => {
+    navbar.classList.toggle('active')
+    search.classList.remove('active')
+    cart.classList.remove('active')
+    user.classList.remove('active')
+}
+
+window.onscroll= () => {
+  document.querySelector('#menu-icon').onclick =() => {
+    search.classList.remove('active')
+    cart.classList.remove('active')
+    user.classList.remove('active')
+    navbar.classList.remove('active')
+}
+}
+ //nav bar scroll
+let header = document.querySelector('header')
+window.addEventListener('scroll',()=>{
+  header.classList.toggle('shadow', window.scrollY > 0)
+})
+// Initialize Swiper 
+
+var swiper = new Swiper(".new-arrival", {
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  centerdSlides: true,
+  breakpoint:{
+    0:{
+      slidesPerView: 0,
+    },
+    568:{
+      slidesPerView: 1,
+  },
+  768:{
+    slidesPerView: 1,
+},
+1020:{
+  slidesPerView:1,
+},
+  },
+},
+);
